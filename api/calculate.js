5f39f677806c950c1a83da2abc8fd480bbe8530f
CQ <38405415+nightwalker89@users.noreply.github.com>
2021-11-09 12:06:35 +0700

    fix cors

--- api/calculate.js
+++ api/calculate.js
@@ -1,6 +1,17 @@
 export default function calculate(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
     res.statusCode = 200;
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return
+    }
+
     let { bill, tipPercent, people } = req.query;
 
 
